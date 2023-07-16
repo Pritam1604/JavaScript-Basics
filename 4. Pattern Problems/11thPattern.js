3f7f6fef23a40f3fe55dde4bc29c627daf9ab47d
Pritam1604 <pritammatghj@gmail.com>
2023-07-16 16:10:44 +0530

    11th

---
+++ 4. Pattern Problems/11thPattern.js
@@ -0,0 +1,42 @@
+// // let n = 5;
+// for (let forRows = 1; forRows <= 2 * n - 1; forRows++) {
+//     if (forRows <= n) {
+//         for (let space1 = 1; space1 <= n - forRows - 1; space1++) {
+//             document.write("&nspb ");
+//         }
+//         for (let forCols = 1; forCols <= 2 * i - 1; forCols++) {
+//             document.write("*");
+//         }
+//         for (let space2 = 1; space2 < n - forRows - 1; space2++) {
+//             document.write(" ");
+//         }
+//     }
+//     else {
+//         for (let space1 = 1; space1 <= forRows; space1++) {
+//             document.write("&nspb&nspb");
+//         }
+//         for (let forCols = 1; forCols <= 2 * i - 1; forCols++) {
+//             document.write(" ");
+//         }
+//         for (let space2 = 1; space2 <= forRows ; space2++) {
+//             document.write(" ");
+//         }
+//     }
+//     document.write("<br/>");
+
+
+
+
+
+// let n=5;
+// for(let forRows=1; forRows<=n; forRows++){
+//     for(let forCols=0; forCols<=forRows; forCols++) document.write(forCols+forRows);
+//     document.write("<br/>");
+// }
+// output
+// 12
+// 234
+// 3456
+// 45678
+// 5678910
+
