bfb6f9cd97671c94a3918adca1b549bc81aa63d0
Pritam1604 <pritammatghj@gmail.com>
2023-07-16 16:06:35 +0530

    1st Pattern Problem

---
+++ 4. Pattern Problems/1stPattern.js
@@ -0,0 +1,26 @@
+// to print
+// *****
+// *****
+// *****
+// *****
+// *****...so on.................
+// 
+// Steps:
+// 1. Eikhane rows=no. of lines jeita holo outer loop
+// 2. Column constant thakche which is 5 bar ar eita insider loop
+// 3. Print korte hobe amader "*".
+
+for (let i = 1; i <= 5; i++) {
+   for (let j = 1; j <= 5; j++) {
+      // console.log("*");
+      document.write("*");
+   }
+   // console.log("\n")
+   document.write("<br/>");
+}
+// Eikhane console e thik bhabe print hoina because of repetition. 
+// tai document.write use korechi so that the thing gets printed on the webpage
+// pore when convenient console.log use korbo.
+
+
+
