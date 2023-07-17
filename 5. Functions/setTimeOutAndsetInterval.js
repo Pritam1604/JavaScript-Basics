25f3d4d9a22e57d79e615517cab04ad679f9d699
Pritam1604 <pritammatghj@gmail.com>
2023-07-17 19:55:55 +0530

    New : Learnt about timeout and Time Interval

---
+++ 5. Functions/setTimeOutAndsetInterval.js
@@ -0,0 +1,22 @@
+// setTimeOut() is function that takes another function in itself with a value which is
+// basically a time after which the function inside is executed.
+
+
+
+
+console.log("Hey Pritam.....");
+
+setTimeout(()=>{
+    console.log("......are you still there???");
+},2000);
+
+setTimeout(()=>{
+    console.log("Goodbye!!!");
+},5000);
+
+
+
+
+// const id = setInterval(()=>{
+//     console.log(Math.floor(Math.random()*6+1));
+// },1000)
